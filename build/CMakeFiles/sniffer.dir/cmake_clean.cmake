d9b57eef0c90860a758b8c432af9fe85671d20b3
Alonso Isidoro Román <alonsoir@gmail.com>
2025-09-23 10:49:43 +0200

    Guardando estado actual de la feature

---
+++ build/CMakeFiles/sniffer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sniffer.dir/proto/network_security_v31.pb.cc.o"
+  "CMakeFiles/sniffer.dir/proto/network_security_v31.pb.cc.o.d"
+  "CMakeFiles/sniffer.dir/src/userspace/config_manager.cpp.o"
+  "CMakeFiles/sniffer.dir/src/userspace/config_manager.cpp.o.d"
+  "CMakeFiles/sniffer.dir/src/userspace/main.cpp.o"
+  "CMakeFiles/sniffer.dir/src/userspace/main.cpp.o.d"
+  "sniffer"
+  "sniffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sniffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
